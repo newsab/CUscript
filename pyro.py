a8fd2a96e7f3da1ad1933f40de13d7bf3f7973a1
Mats Nilsson <matsnilsson87@gmail.com>
2021-03-17 16:34:46 +0100

    pyro fix

--- pyro.py
+++ pyro.py
@@ -1,5 +1,4 @@
 import Pyro4
 # use the URI that the server printed:
-uri = "PYRO:Pyro.NameServer@172.16.0.3:34291"
-thing = Pyro4.Proxy(uri)
-print(thing.method(42)) 
+thing = Pyro4.Proxy("PYRONAME:myrtk")
+print(thing.getPosition())   # prints 84
