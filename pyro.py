cec170c631b1818b960cf6fe444dc3a84138ad06
Mats Nilsson <matsnilsson87@gmail.com>
2021-03-19 14:38:29 +0100

    s

--- pyro.py
+++ pyro.py
@@ -1,4 +1,40 @@
 import Pyro4
+import threading
+import time
 # use the URI that the server printed:
-thing = Pyro4.Proxy("PYRONAME:myrtk")
-print(thing.getPosition())   # prints 84
+
+
+class StartAndStop:
+
+    def __init__(self):
+        self.quitflag = False
+        self.quitlock = threading.Lock()
+        self.thing = Pyro4.Proxy("PYRONAME:PMUApp")
+
+    def start(self):
+        while True:
+            with self.quitlock:
+                if self.quitflag:
+                    return
+            self.thing.startMeasure()
+
+    def stop(self):
+        with self.quitlock:
+            self.quitflag = True
+        self.thing.stopMeasure()
+        t.join()
+
+    t = threading.Thread(target=start)
+    t.start()
+
+
+def test():
+    print("test1")
+    p = StartAndStop()
+   # p.start()
+    time.sleep(5)
+    print("HEEEJEEJEJE")
+    p.stop()
+
+
+test()
