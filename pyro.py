3cdf4736ddc7d047dfc2e5dd8e253dd524c4439a
Mats Nilsson <matsnilsson87@gmail.com>
2021-03-22 20:51:21 +0100

    Pyro fix

--- pyro.py
+++ pyro.py
@@ -13,24 +13,23 @@ class StartAndStop:
         self.quitflag = False
         self.quitlock = threading.Lock()
         self.thing = Pyro4.Proxy("PYRONAME:PMUApp")
-        #self.py = self.thing.__copy__()
         self.t = threading.Thread(target=self.start2)
 
     def start2(self):
         print("Hej från tråden")
-        self.thing.startaStart()
+        self.thing.starta()
 
     def stop(self):
-        with self.quitlock:
-            if self.quitflag:
-                print("Hej tråden ska vi nysta?")
-                # self.t._stop()
-                self.thing.stopMeasure()
-                return
-
+        print("Hej tråden ska vi nysta?")
+        # self.t.join()
+        self.thing.stopMeasure()
+        print("Progress")
+        return
         print("Måste starta först!")
 
     def start(self):
-        with self.quitlock:
-            self.quitflag = True
-        self.t.start()
+        if self.t.is_alive():
+            print("test")
+            self.t.start()
+        else:
+            self.start2()
