5bb8a2bd6da2cd69acac68a50af7e8adb399536d
cfenerholm <cf.enerholm@gmail.com>
2021-03-31 13:22:21 +0200

    Lite städning

--- GUI.py
+++ GUI.py
@@ -8,7 +8,7 @@ from Measurements import *
 from pyro import *
 from ShellCommands import *
 
-global on
+global on 
 global a
 global x
 global y
@@ -16,7 +16,6 @@ global z
 global m
 global my_cmap
 global sas
-#global figLive
 global scttLive
 global canvasLive
 
@@ -27,48 +26,33 @@ y = []
 z = []
 m = []
 sas = object
-#figLive = object
 scttLive = object
 canvasLive = object
 my_cmap = plt.get_cmap('rainbow')
-pmuSc = ShellCommands("172.16.0.3")
+pmuSc = ShellCommands("81.16.170.67")
 rbuSc = ShellCommands("172.16.0.6")
 ptuSc = ShellCommands("172.16.0.9")
-#pmuIp = "172.16.0.3"
-#rbuIp = "172.16.0.6"
-
-# print(sas)
-
 
 def clickStartBtn():
+    """
+    Comment
+    """
     global on
     global a
-    global x
-    global y
-    global z
-    global m
     global sas
     if not on:
-        sas = StartAndStop()
-        on = True
         startBtn.config(text="Stoppa mätning")
+        on = True
+        sas = StartAndStop()
         frequency = fqEnt.get()
         sas.start(frequency)
         time.sleep(3.5)
-        # createLiveFig()
         while on:
             showList = sas.showList
             obj = showList[-1]
             tbMeasure.insert(1.0, str(obj) + '\n')
             tbMeasure.update()
-            lo = obj[1]
-            la = obj[2]
-            al = obj[3]
-            me = obj[4]
-            x.append(float(lo))
-            y.append(float(la))
-            z.append(float(al))
-            m.append(float(me))
+            updatePlotList(obj)
             livePlot()
             time.sleep(0.5)
         # ptuSc.setFrequency(frequency)
@@ -76,48 +60,19 @@ def clickStartBtn():
         startBtn.config(text="Starta mätning")
         on = False
         sas.stop()
-        x[:] = []
-        y[:] = []
-        z[:] = []
-        m[:] = []
-        a = sas.mesurement  # change to createDummy() to run txt-file
+        clearPlotLists()
+        a = sas.mesurementList  # change to createDummy() to run txt-file
         print("list done!")
         for line in a:
-            lo = line[1]
-            la = line[2]
-            al = line[3]
-            me = line[4]
-            x.append(float(lo))
-            y.append(float(la))
-            z.append(float(al))
-            m.append(float(me))
+            updatePlotList(line)
             tbMeasure.insert(1.0, str(line) + '\n')
             tbMeasure.update
-            # time.sleep(3)
-            #tbMeasure.delete(1.0, END)
         del sas
 
-
-"""
-def updateMeasurement():
-
-    line = sas.obj
-    lo = line[1]
-    la = line[2]
-    al = line[3]
-    me = line[4]
-    x.append(float(lo))
-    y.append(float(la))
-    z.append(float(al))
-    m.append(float(me))
-
-    tbMeasure.insert(1.0, str(line) + '\n')
-    tbMeasure.update()
-"""
-
-
 def clickPmuBtn():
-
+    """
+    Comment
+    """
     msg2 = pmuSc.startStr2StrClient()
     tbOthers.insert(1.0, msg2 + '\n \n')
     tbOthers.update()
@@ -136,8 +91,10 @@ def clickPmuBtn():
     tbOthers.insert(1.0, 'PMU Ready for take off! \n \n')
     tbOthers.update()
 
-
 def clickPosBtn():
+    """
+    Comment
+    """
     sas = StartAndStop()
     startPosition = sas.getStartPosition()
     lon = startPosition[0]
@@ -149,8 +106,10 @@ def clickPosBtn():
     tbOthers.update()
     del sas
 
-
 def clickRbuBtn():
+    """
+    Comment
+    """
     msg = rbuSc.coldRestart()
     tbOthers.insert(1.0, msg + '\n \n')
     tbOthers.update()
@@ -169,15 +128,19 @@ def clickRbuBtn():
     tbOthers.insert(1.0, 'RBU Ready DO NOT MOVE!!! \n \n')
     tbOthers.update()
 
-
 def clickGrafBtn():
+    """
+    Comment
+    """
     fig = plt.figure(figsize=(10, 7))
     sctt = plt.scatter(x, y, alpha=1, c=m, cmap=my_cmap, marker='o')
     fig.colorbar(sctt, shrink=0.8, aspect=5)
     fig.show()
 
-
 def clickGraf3dBtn():
+    """
+    Comment
+    """
     fig = plt.figure(figsize=(15, 11))
     ax = plt.axes(projection="3d")
     sctt = ax.scatter3D(x, y, z, alpha=1, c=m, cmap=my_cmap, marker='p')
@@ -187,19 +150,10 @@ def clickGraf3dBtn():
     fig.colorbar(sctt, ax=ax, shrink=0.8, aspect=5)
     fig.show()
 
-
-bgColor = 'black'
-frameColor = '#222222'
-textColor = '#cdcdcd'
-
-win = Tk()
-win.title("CU-applikation för PAMP")
-
-win.geometry('1400x800')
-win.configure(bg=bgColor)
-
-
 def createLiveFig():
+    """
+    Comment
+    """
     global scttLive
     global canvasLive
     figLive = plt.figure(figsize=(5, 3))
@@ -208,62 +162,86 @@ def createLiveFig():
     canvasLive = FigureCanvasTkAgg(figLive, master=win)
     canvasLive.get_tk_widget().grid(row=0, column=15)
 
-
 def livePlot():
+    """
+    Comment
+    """
     global scttLive
     global canvasLive
     scttLive = plt.scatter(x, y, alpha=1, c=m, cmap=my_cmap, marker='o')
     canvasLive.draw_idle()
 
+def clearPlotLists():
+    """
+    Comment
+    """
+    global x
+    global y
+    global z
+    global m
+    x[:] = []
+    y[:] = []
+    z[:] = []
+    m[:] = []
 
-# def updateLivePlotter():
-    #scttLive = plt.scatter(x, y, alpha=1, c=m, cmap=my_cmap, marker='o')
-    # canvasLive.draw_idle()
+def updatePlotList(line):
+    """
+    Comment
+    """
+    lo = line[1]
+    la = line[2]
+    al = line[3]
+    me = line[4]
+    x.append(float(lo))
+    y.append(float(la))
+    z.append(float(al))
+    m.append(float(me))
 
+bgColor = 'black'
+frameColor = '#222222'
+textColor = '#cdcdcd'
 
-tbMeasure = Text(bg=frameColor, fg=textColor, width=90)
-tbOthers = Text(bg=frameColor, fg=textColor, width=60)
+win = Tk()
+win.title("CU-applikation för PAMP")
+win.geometry('1448x800')
+win.configure(bg=bgColor)
 
-startLbl = Label(text="Starta mätning", bg=bgColor, fg=textColor)
-startBtn = Button(text="Starta", width=20, height=2,
+tbMeasure = Text(bg=frameColor, fg=textColor, width=85)
+
+tbOthers = Text(bg=frameColor, fg=textColor, width=50)
+
+startLbl = Label(text="", bg=bgColor, fg=textColor)
+
+startBtn = Button(text="Starta", width=15, height=2,
                   bg=frameColor, fg=bgColor, command=clickStartBtn)
 
-#brLbl = Label(text="Sätt baudrate", bg=bgColor, fg=textColor)
-#brEnt = Entry(bg=frameColor, fg=textColor)
-pmuBtn = Button(text="Starta PMU", width=20, height=2,
+pmuBtn = Button(text="Starta PMU", width=15, height=2,
                 bg=frameColor, fg=bgColor, command=clickPmuBtn)
 
-#fqLbl = Label(text="Sätt mätrekvens", bg=bgColor, fg=textColor)
 fqEnt = Entry(bg=frameColor, fg=textColor)
-rbuBtn = Button(text="Start Rbu", width=20, height=2,
+
+rbuBtn = Button(text="Start Rbu", width=15, height=2,
                 bg=frameColor, fg=bgColor, command=clickRbuBtn)
 
-grafBtn = Button(text="Visa Graf", width=20, height=2,
+grafBtn = Button(text="Visa Graf", width=15, height=2,
                  bg=frameColor, fg=bgColor, command=clickGrafBtn)
 graf3dBtn = Button(text="Visa 3D Graf", width=20, height=2,
                    bg=frameColor, fg=bgColor, command=clickGraf3dBtn)
 
 posLbl = Label(text="", bg=frameColor, fg=textColor)
-posBtn = Button(text="Ta ut AUT position", width=20, height=2,
+
+posBtn = Button(text="Ta ut AUT position", width=15, height=2,
                 bg=frameColor, fg=bgColor, command=clickPosBtn)
 
 tbOthers.grid(row=0, column=0, columnspan=3)
 tbMeasure.grid(row=0, column=3, columnspan=3)
-
 startLbl.grid(row=1, column=1)
 startBtn.grid(row=1, column=2)
-
-#brLbl.grid(row=2, column=1)
-#brEnt.grid(row=3, column=1)
 pmuBtn.grid(row=3, column=2)
-
-#fqLbl.grid(row=4, column=1)
 fqEnt.grid(row=5, column=1)
 rbuBtn.grid(row=5, column=2)
-
 grafBtn.grid(row=5, column=4)
 graf3dBtn.grid(row=5, column=5)
-
 posLbl.grid(row=6, column=1, columnspan=4)
 posBtn.grid(row=7, column=4)
 
--- pyro.py
+++ pyro.py
@@ -2,47 +2,69 @@ import threading
 import time
 from logging import shutdown
 from os.path import commonpath, exists, lexists
-
 import Pyro4
-
-# use the URI that the server printed:
-
 
 class StartAndStop:
 
     def __init__(self):
-        self.quitflag = False
-        self.freq = 0.0
-        self.quitlock = threading.Lock()
-        self.thing = Pyro4.Proxy("PYRONAME:PMUApp")
-        self.t = threading.Thread(target=self.start2)
-        self.mesurement = list
-        self.showList = list
-        print(self.thing)
+        """
+        Creates a StartAndStop object which can communicate with the PMU. 
+        The object should be killed after use with "del "object name"" function.
+        """
+        self.quitflag = False                                       #Creates a boolean which is used to show if the measurementloop should stop or countinue
+        self.quitlock = threading.Lock()                            #Creates a threading lock
+        self.pmu = Pyro4.Proxy("PYRONAME:PMUApp")                   #Creates the Pyro proxy
+        self.t = threading.Thread(target=self.startPmuMeasurement)  #Creates a thread with startPmuMeasurement as target
+        self.freq = 0.0                                             #Creates a float to store the frequency in  
+        self.mesurementList = list                                  #Creates a list to store the complete measurementdata
+        self.showList = list                                        #Creates a list to store a smaller amount of measurementdata for the live plot in GUI
 
-    def start2(self):
-        self.quitflag = False
-        print("Hej från tråden")
-        self.thing.starta(self.freq)
-        time.sleep(2)
-        print("Jag kom forbi!!!!")
-        while not self.quitflag:
-            self.showList = self.thing.getListToSend()
-            obj = self.showList[-1]
-            print(obj)
-            time.sleep(0.3)
+    def startPmuMeasurement(self):
+        """
+        Calling the PMU function that starts the measurment and runs the 
+        loop to update the showList as long the quitflag is set to False. 
+        """
+        try:
+            self.quitflag = False
+            self.pmu.starta(self.freq)
+            time.sleep(2)
+            while not self.quitflag:
+                self.showList = self.pmu.getListToSend()
+                time.sleep(0.3)
+        except:
+            print('Could not run function startPmuMeasurement from StartAndStop')
 
     def stop(self):
-        print("Hej tråden ska vi nysta?")
-        self.quitflag = True
-        self.mesurement = self.thing.stopMeasure()
-        print("Progress when ju need sucess")
-        return self.mesurement
+        """
+        Calling the PMU function that stops the measurement and returns 
+        the measurementList which includes all measurements made by the PMU. 
+        Also sets the quitflag to True to stop the update of the showList.
+        """
+        try:
+            self.quitflag = True
+            self.mesurementList = self.pmu.stopMeasure()
+            return self.mesurementList
+        except:
+            print('Could not run function stop from StartAndStop')
 
     def start(self, frequency):
-        self.freq = frequency
-        self.t.start()
+        """
+        Takes a frequency as parameter and starts the theard 
+        which is calling the startPmuMeasurement.
+        """
+        try:
+            self.freq = frequency
+            self.t.start()
+        except:
+            print('Could not run function start from StartAndStop')
 
     def getStartPosition(self):
-        startPosition = self.thing.getStartPosition()
-        return startPosition
+        """
+        Asks PMU for singelposition and returns object including longitude, 
+        latitude and altitude witch is used to get the AUTs position.
+        """
+        try:
+            startPosition = self.pmu.getStartPosition()
+            return startPosition
+        except:
+            print('Could not run function getStartPosition from StartAndStop')
